5e645b3a5c6a0e486be8b1a354a510692f756697
Mr3404 <allaberdi16yazhanow@gmail.com>
2024-10-29 23:17:31 +0400

    profile model creater

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name="user", related_name="user")
+    firstname = models.CharField(max_length=20, verbose_name="First Name", blank=True, null=True)
+    lastname = models.CharField(max_length=20, verbose_name="Last Name", blank=True, null=True)
+    mobile = models.CharField(max_length=12, verbose_name="mobile phone", blank=True, null=True)
+
+
+    class Meta:
+        verbose_name_plural = "Profiles"
+
+    
+    def __str__(self) -> str:
+        return str(self.firstname)
+    
+    @receiver(post_save, sender=User)
+    def create_user_info(sender, instance, created, **kwargs):
+        if created:
+            profile = Profile(user=instance)
+            profile.save()
